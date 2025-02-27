dbfa3f06b30bf5faf00e8339856660fb738403fe
Jefter Alves <jefteralveskf2@gmail.com>
2025-02-27 00:41:15 -0300

    feat(Product filter): Listing products by category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,7 @@ import { ListCategoryController } from "./controllers/category/ListCategoryContr
 import { CreateProductController } from "./controllers/product/CreateProductController";
 
 import uploadConfig from "./config/multer";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
 
@@ -30,6 +31,6 @@ router.get('/listcategory', isAuthenticated, new ListCategoryController().handle
 
 //-- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
-
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router };
