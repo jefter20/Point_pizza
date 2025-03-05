1333d6eb50497eff216f80e5e36846840f0ce000
Jefter Alves <jefteralveskf2@gmail.com>
2025-03-05 18:57:13 -0300

    feat(login): Globals settings and login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,44 @@
 import styles from './page.module.scss';
+import logoImg from '/public/logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return(
-    <main>
-      <h1 className={styles.cor} >TESTE</h1>
-    </main>
+    <>
+      <div className={ styles.containerCenter } >
+        <Image
+          src={ logoImg }
+          alt="Logo da pizzaria"
+        />
+
+        <section className={ styles.login }>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={ styles.input }
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="*****"
+              className={ styles.input }
+            />
+            <button type="submit">
+              Acessar
+            </button>
+          </form>
+
+          <Link href="/signup" className={ styles.text }>
+            Não possue uma conta? Cadastre-se
+          </Link>
+
+        </section>
+      </div>
+    </>
   )
 }
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+    return(
+        <>
+            <main>
+                <h1>PAGINA DE CADASTRO</h1>
+            </main>
+        </>
+    )
+}
