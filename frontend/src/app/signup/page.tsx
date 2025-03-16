0abccaa18f78f8d81572d34efe65f4086e9222b2
Jefter Alves <jefteralveskf2@gmail.com>
2025-03-15 22:53:14 -0300

    feat(signup): Update: saving user in the bank

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,8 +2,46 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../page.module.scss';
 import logoImg from '/public/logo.svg';
+import { api } from '@/services/api';
+import { redirect } from 'next/navigation';
 
 export default function Signup() {
+
+    async function handleRegister(formData: FormData) {
+
+        "use server"
+
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const repeatPassword = formData.get("repeatPassword");
+
+        if(name === "" || email === "" || password === "" || repeatPassword === "") {
+            console.log("Preencha todos os campos!");
+            return;
+        }
+
+        try{
+
+            await api.post("/users", {
+                name,
+                email,
+                password,
+                repeatPassword
+            })
+
+        }
+        catch(err) {
+
+            console.log("error")
+            console.log(err)
+            
+        }
+
+        redirect("/");
+        
+    }
+
     return(
         <>
             <div className={ styles.containerCenter } >
@@ -14,7 +52,7 @@ export default function Signup() {
 
                 <section className={ styles.login }>
                     <h1>Cadastre-se</h1>
-                    <form>
+                    <form action={ handleRegister }>
                         <input
                         type="text"
                         required
@@ -39,7 +77,7 @@ export default function Signup() {
                         <input
                         type="password"
                         required
-                        name="password"
+                        name="repeatPassword"
                         placeholder="Repita sua senha"
                         className={ styles.input }
                         />
